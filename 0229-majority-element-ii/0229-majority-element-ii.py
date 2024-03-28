2557600db98928ce3194ad5382298f94ded218be
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-03-28 19:09:03 -0400

    Time: 109 ms (13.74%), Space: 17.9 MB (27.69%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        # time complexity: O(n)
+        my_dict = {}
+        res = []
+        n = len(nums)
+
+        if n <= 1:
+            return nums
+
+        for num in nums:
+            if num in my_dict:
+                my_dict[num] += 1 
+            else:
+                my_dict[num] = 1 
+            
+        # to check the occurence if it is greater than n/3
+        for key, val in my_dict.items():
+            if val > n/3:
+                res.append(key)
+        
+        return res
