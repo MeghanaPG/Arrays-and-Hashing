4ab848d462c7098c8f6a5051a8c1e06b5fd2b321
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-08-18 11:21:21 -0400

    Time: 40 ms (48.31%), Space: 16.6 MB (43.22%) - LeetHub

---
+++ 1085-sum-of-digits-in-the-minimum-number/1085-sum-of-digits-in-the-minimum-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sumOfDigits(self, nums: List[int]) -> int:
+        # Time Complexity:
+
+        min_num = min(nums)
+    
+        numSum = sum(int(digit) for digit in str(min_num))
+
+        return 0 if numSum % 2 != 0 else 1
