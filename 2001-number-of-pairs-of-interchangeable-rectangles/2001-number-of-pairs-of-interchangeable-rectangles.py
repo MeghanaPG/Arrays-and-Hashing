31cac3d1bf8df8227b6865edd3a0ec33d49c37e4
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-09-04 07:03:31 -0400

    Time: 1326 ms (53.00%), Space: 72.8 MB (38.28%) - LeetHub

--- 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.py
+++ 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.py
@@ -1,11 +1,12 @@
 class Solution:
     def interchangeableRectangles(self, rectangles: List[List[int]]) -> int:
+        # Time Complexity: O(n)
+        # for each ratio we will store it's count
         myHash = defaultdict(int)
         res = 0 
 
-        for a, b in rectangles:
-            ratio = a / b 
+        for w,h in rectangles:
+            ratio = w/h
             res += myHash[ratio]
             myHash[ratio] += 1
-
-        return res
+        return res 
