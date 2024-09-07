ed4d45a35ab0c894ed4bd2d42eed5d7eb180e724
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-09-07 10:16:02 -0400

    Time: 87 ms (70.01%), Space: 19.4 MB (88.00%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,7 +2,7 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         if len(strs) == 1:
             return [strs]
-        
+
         sorted_str_list = []
 
         anagram_hash_table = {}
@@ -18,6 +18,7 @@ class Solution:
                 anagram_hash_table[key].append(strs[i])
             else:
                 anagram_hash_table[key] = [strs[i]]
-        
+                
         return list(anagram_hash_table.values())
-            
+
+
