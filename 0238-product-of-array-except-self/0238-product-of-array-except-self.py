515d19ce26e4c3ecca05de31599721de1433c8f6
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-10-08 07:16:34 -0400

    Time: 296 ms (9.99%), Space: 25.7 MB (88.18%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -7,13 +7,12 @@ class Solution:
 
         for i in range(1, len(nums)):
             prefix_arr[i] = prefix_arr[i-1] * nums[i-1]
-
         
         for i in range(len(nums)-2, -1, -1):
             postfix_arr[i] = postfix_arr[i+1] * nums[i+1]
-        
 
         for i in range(len(nums)):
             res[i] = prefix_arr[i] * postfix_arr[i]
         
-        return res
+        return res 
+
