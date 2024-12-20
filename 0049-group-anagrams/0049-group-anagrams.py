f0d2758e37f59a86240f3fafe86d8fd6007c5cd6
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-12-20 08:18:15 -0500

    Time: 8 ms (91.03%), Space: 21 MB (36.67%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -12,13 +12,11 @@ class Solution:
             str_list.sort()
             sorted_str = "".join(str_list)
             sorted_str_list.append(sorted_str)
-        
+
         for i, key in enumerate(sorted_str_list):
             if key in anagram_hash_table:
                 anagram_hash_table[key].append(strs[i])
             else:
                 anagram_hash_table[key] = [strs[i]]
-                
+        
         return list(anagram_hash_table.values())
-
-
